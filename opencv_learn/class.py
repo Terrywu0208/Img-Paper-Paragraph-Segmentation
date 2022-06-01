84d93a0d0aec1c6dfc4053c49928b3e1d15b37f2
Terrywu0208 <75594915+Terrywu0208@users.noreply.github.com>
2022-06-01 14:38:18 +0800

    20220601

--- opencv_learn/class.py
+++ opencv_learn/class.py
@@ -76,7 +76,8 @@ def usual():
     # cv2.imshow("erode",erode)
     # new = dilate[1]
     arr = np.array(img)
-    # arr = arr[100:237]
+    # arr = arr[100:237,220:505]
+    arr = arr[142:186,220:505]
     temp = []
     for i in range(len(arr)):
         for j in range(len(arr[i])):
@@ -88,94 +89,9 @@ def usual():
                     pass
                 temp.append(i)
             
-    # cv2.imshow("erode",arr)
+    cv2.imshow("erode",arr)
     # print(len(arr))
-    # cv2.waitKey(0)
-
-def draw():
-    img = np.zeros((600,600,3),np.uint8)
-
-    cv2.line(img, (0,0), (img.shape[1], img.shape[0]), (0, 0, 255), 2)
-    cv2.rectangle(img, (0, 0), (400, 300),(0,255,0), cv2.FILLED)
-    cv2.circle(img, (300, 400) ,30 ,(200,200,200),2)
-    cv2.putText(img, "Hi" ,(100,500), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2 , (255,255,255), 2)
-
-    cv2.imshow("img", img)
     cv2.waitKey(0)
-
-# def filter_color():
-def empty(v):
-    pass
-
-# img = cv2.imread(r"img\winnie.jpg")
-# img = cv2.resize(img,(0,0), fx=0.5, fy=0.5)
-# cap = cv2.VideoCapture(0)
-
-
-# cv2.namedWindow("TrackBar")
-# cv2.resizeWindow("TrackBar", 640, 320)
-
-# cv2.createTrackbar("Hue Min" , "TrackBar", 0, 179, empty)
-# cv2.createTrackbar("Hue Max" , "TrackBar", 177, 179, empty)
-# cv2.createTrackbar("Sat Min" , "TrackBar", 0, 255, empty)
-# cv2.createTrackbar("Sat Max" , "TrackBar", 255, 255, empty)
-# cv2.createTrackbar("Val Min" , "TrackBar", 0, 255, empty)
-# cv2.createTrackbar("Val Max" , "TrackBar", 255, 255, empty)
-
-def detcet_geometry():
-    img = cv2.imread(r"img\geometry.png")
-    img = cv2.resize(img,(0,0), fx=2, fy=2)
-    imgContour = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    canny = cv2.Canny(img,150,200)
-    contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    for cnt in contours:
-        cv2.drawContours(imgContour, cnt, -1,(255, 0,0), 4)
-        area = cv2.contourArea(cnt)
-        if area >200:
-            peri = cv2.arcLength(cnt, True)
-            vertices = cv2.approxPolyDP(cnt, peri*0.02, True)
-            corners = len(vertices)
-            x, y, w, h = cv2.boundingRect(vertices)
-            cv2.rectangle(imgContour, (x, y), (x+w, y+h), (0, 255, 0), 4)
-            if corners == 3:
-                cv2.putText(imgContour,"Triangle", (x, y-5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0,0,255),2)
-            elif corners == 4:
-                cv2.putText(imgContour,"Rectangle", (x, y-5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0,0,255),2)
-            elif corners == 5:
-                cv2.putText(imgContour,"Pentagon", (x, y-5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0,0,255),2)
-            elif corners >6 :
-                cv2.putText(imgContour,"Circle", (x, y-5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0,0,255),2)
-
-    cv2.imshow("img", img)
-    cv2.imshow("canny",canny)
-    cv2.imshow("imgContour", imgContour)
-    cv2.waitKey(0)
-
-# while True:
-#     # ref, img = cap.read()
-#     img = cv2.imread("img/paper0001-06.jpg")
-#     img = cv2.resize(img,None,fx=0.2,fy=0.2,interpolation=cv2.INTER_LINEAR)
-#     hsv = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
-#     h_min = cv2.getTrackbarPos("Hue Min" , "TrackBar")
-#     h_max = cv2.getTrackbarPos("Hue Max" , "TrackBar")
-#     s_min = cv2.getTrackbarPos("Sat Min" , "TrackBar")
-#     s_max = cv2.getTrackbarPos("Sat Max" , "TrackBar")
-#     v_min = cv2.getTrackbarPos("Val Min" , "TrackBar")
-#     v_max = cv2.getTrackbarPos("Val Max" , "TrackBar")
-#     print(h_min, h_max, s_min, s_max, v_min, v_max)
-
-#     lower = np.array([h_min, s_min, v_min])
-#     upper = np.array([h_max, s_max, v_max])
-
-#     mask = cv2.inRange(hsv , lower, upper)
-
-#     result = cv2.bitwise_and(img, img, mask = mask)
-
-#     cv2.imshow("img",img)
-#     cv2.imshow("result",result)
-#     cv2.waitKey(10)
         
 
 usual()
